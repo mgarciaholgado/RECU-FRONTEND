5fcd996a558eeab4952bb5f2f4fa64fa8fe6b5b8
Miguel García Holgado <91050890+mgarciaholgado@users.noreply.github.com>
2022-06-02 21:36:15 +0200

    versionFinal

--- dist/proyecto-fronted/app/models/cliente.js
+++
@@ -1,8 +0,0 @@
-export class Clientes {
-    constructor(dni, nombre, telefono) {
-        this._dni = dni,
-            this._nombre = nombre,
-            this._telefono = telefono;
-    }
-}
-//# sourceMappingURL=cliente.js.map
--- dist/proyecto-fronted/app/models/reparacion.js
+++
@@ -1,9 +0,0 @@
-export class Reparaciones {
-    constructor(codigo, matricula, nombre, coste) {
-        this._codReparacion = codigo;
-        this._matricula = matricula;
-        this._nombreReparacion = nombre;
-        this._CosteBase = coste;
-    }
-}
-//# sourceMappingURL=reparacion.js.map
